ce561bf1a6f961a70a4890e0314f4022d5b57ff9
TheRogueNinja <shreyassawant018@gmail.com>
2017-08-03 16:21:20 +0530

    Welcome Scene added

---
+++ MatchingTiles/Assets/Scripts/SceneMan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneMan : MonoBehaviour {
+	[SerializeField]
+	private GameObject Tile;
+	[SerializeField]
+	private GameObject Slide;
+	
+	public Animator anim;
+	public void loadMatchTiles(){
+		SceneManager.LoadScene(1);
+	}
+	void OnMouseOver(){
+		anim.Play("TileFlip");
+		anim.Play("TileFlip");
+	}
+}
